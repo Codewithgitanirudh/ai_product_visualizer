2331647f64b86b814a9fb7514e2bcacf70bcfcf1
anirudh <anirudhjangid0@gmail.com>
2025-11-25 22:23:55 +0530

    Adjust padding in Header component for improved layout consistency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 export default function Header () {
   return (
-    <header className="md:border-b md:border-base-400 d:shadow-sm sticky top-0 z-40 md:backdrop-blur-sm md:bg-white/95 md:pl-0 pl-12">
+    <header className="md:border-b md:border-base-400 d:shadow-sm sticky top-0 z-40 md:backdrop-blur-sm md:bg-white/95 md:pl-0 pl-10">
       <div className="container mx-auto px-4 md:px-8 md:py-5 py-3 flex items-center justify-between">
         <Link to="/" className="flex items-center space-x-3 hover:opacity-80 transition-opacity">
           <div className="p-2.5 bg-linear-to-br from-brand-primary via-brand-secondary to-brand-accent rounded-xl shadow-md">
